d01bd394c7140b0b38708a4e8d16adf9a485b41c
starkperez <168944563+starkperez@users.noreply.github.com>
2025-10-18 10:58:35 -0400

    Add files via upload

---
+++ bucles.py
@@ -0,0 +1,17 @@
+for i in range(11):
+     print (f"Iteración {i + 1}")
+amigos = ["maicol, ediel, abraham"] 
+
+for  amigo in amigos:
+    print (f"mis amigos son, {amigo}")
+    
+    for i in range(11):
+     print (f"Iteración {i + 1}")
+     
+     edad = 0
+     while edad <11:
+         print (f"edad: {edad}")
+         edad += 1
+         
+     
+     
